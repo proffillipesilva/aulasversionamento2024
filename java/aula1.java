b3dd2c4424643305b5ef35b32a067a090e8349a3
proffillipesilva <81185103+proffillipesilva@users.noreply.github.com>
2024-08-02 22:41:19 -0300

    Create aula1.java

---
+++ java/aula1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int soma = calculaSomaDosMultiplosDeCincoEImpar(n);
+        System.out.println(soma);
+    }
+
+    public static int calculaSomaDosMultiplosDeCincoEImpar(int n){
+        int soma = 0;
+        for(int i=1; i<=n; i++){
+            if(i % 2 != 0 || i % 5 == 0){
+                soma += i;
+            }
+        }
+        return soma;
+    }
+}
