47034ff59de75436debaeaec79ceeda84fdfc77b
proffillipesilva <81185103+proffillipesilva@users.noreply.github.com>
2024-08-27 20:48:06 -0300

    Feature aula docker compose (#19)
    * Extra1
    
    * Extra2
    
    * Resolvendo conflito
    
    * Adicionando estilo
    
    * Adicionando gitignore
    
    * Adicionando codigo do banco de dados
    
    ---------
    
    Co-authored-by: Fillipe de Souza Silva <fssilva@fiecalunos.lan>

--- java/aula1.java
+++ java/aula1.java
@@ -1,22 +1,44 @@
-import java.util.Scanner;
+import org.junit.jupiter.api.Test;
 
-// Esse eh um comentario feliz
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int soma = calculaSomaDosMultiplosDeCincoEImpar(n);
-        System.out.println(soma);
+public class Testes {
+    @Test
+    void test(){
+        int a = 3;
+        Cachorro dog1 = new Cachorro();
+        Cachorro dog2 = new Cachorro();
+        dog1.setRaca("Golden");
+        dog2.setRaca("Pug");
+        System.out.println("Dog1: " + dog1.getRaca() + " Dog2: " + dog2.getRaca());
     }
 
-    // Essa funcao eh legal
-    public static int calculaSomaDosMultiplosDeCincoEImpar(int n){
-        int soma = 0;
-        for(int i=1; i<=n; i++){
-            if(i % 2 != 0 || i % 5 == 0){
-                soma += i;
-            }
+    class Cachorro {
+        // Atributos
+        String nome;
+        String raca;
+        int idade;
+
+        public int getIdade() {
+            return idade;
         }
-        return soma;
+
+        public String getNome() {
+            return nome;
+        }
+
+        public String getRaca() {
+            return raca;
+        }
+
+        public void setIdade(int idade) {
+            this.idade = idade;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public void setRaca(String raca) {
+            this.raca = raca;
+        }
     }
 }
