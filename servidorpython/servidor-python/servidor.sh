e96a1ddebf6a55f4af9455650cdb11187c9c9403
proffillipesilva <81185103+proffillipesilva@users.noreply.github.com>
2024-09-10 20:03:29 -0300

    Fix docker 1 (#24)
    * Update Dockerfile
    
    * Update servidor.sh

--- servidorpython/servidor-python/servidor.sh
+++ servidorpython/servidor-python/servidor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /servicos  # entra na pasta serviços
-unzip ./aula11.zip # descompacta os arquivos
+unzip ./aula11.zip # descompacta os arquivos e os guarda na pasta servicos
 rm aula11.zip  # remove o arquivo .zip após a descompactação
-python3 -m http.server 9000 # roda na porta 9000
+python3 -m http.server 9000 --directory aula11 # roda na porta 9000 e roda no diretorio aula11
